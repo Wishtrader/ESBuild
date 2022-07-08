8b39f301ebe42a1b550828f2cbdd7ffbf9a3934b
Andrei Kamarou <WISHMASTER@Andreis-iMac.local>
2022-07-09 02:56:25 +0300

    tsconfig

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './index.css';
 import './second.css';
 
 const App = () => {
-    const [state, setState] = useState(0)
+    const [state, setState] = useState<number>(0)
     return (
         <div>
          <h1>value = ${state}</h1>
--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,9 @@ import App from './App';
 
 const rootElement = document.getElementById('root');
 
+if(!rootElement) {
+    throw new Error('Root element not found!');
+}
+
 const root = createRoot(rootElement);
 root.render(<App />);
